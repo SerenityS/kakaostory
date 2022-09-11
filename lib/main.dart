1fe18588f5908953a22ee5e9a4155de3b4162a2d
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2022-09-12 02:22:30 +0900

    feat: Implement Login
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:get/get.dart';
 import 'package:kakaostory/controller/auth_controller.dart';
 import 'package:kakaostory/view/splash_view.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isAndroid) {
+    await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
+  }
 
   Get.put(AuthController());
 
---
+++ lib/view/login_view.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:get/get.dart';
+import 'package:kakaostory/const/kakao_urls.dart' as KakaoUrl;
+import 'package:kakaostory/controller/auth_controller.dart';
+import 'package:kakaostory/view/main_view.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({Key? key}) : super(key: key);
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  InAppWebViewController? webViewController;
+  InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
+    android: AndroidInAppWebViewOptions(
+      useHybridComposition: true,
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("카카오스토리 로그인")),
+      body: InAppWebView(
+          initialUrlRequest: URLRequest(url: Uri.parse(KakaoUrl.loginUrl)),
+          onWebViewCreated: (controller) async {
+            webViewController = controller;
+          },
+          onLoadStop: (controller, url) async {
+            if (url!.host.contains("story.kakao.com")) {
+              await AuthController.instance.saveCookies();
+              Get.offAll(() => MainView());
+            }
+          }),
+    );
+  }
+}
