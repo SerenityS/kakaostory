69778e71e2cb2d04a9724e8c4be5418ffb6837c0
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2022-09-12 19:47:19 +0900

    chore: Add emoticon related urls & fix derp
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/const/kakao_urls.dart
+++ lib/const/kakao_urls.dart
@@ -1,3 +1,5 @@
 String loginUrl = "https://accounts.kakao.com/login/?continue=https://story.kakao.com/";
-String mainUrl = "https://story.kakao.com";
+String mainUrl = "https://story.kakao.com/";
 String feedUrl = "https://story.kakao.com/a/feeds";
+String emoticonUrl = "https://mk.kakaocdn.net/dna/emoticons/resources";
+String emoticonAuthUrl = "https://api-item.kakao.com/api/sdk/config";
