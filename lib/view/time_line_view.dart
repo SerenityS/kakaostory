4775bbe867f8a12e531caed9866bbde457dbd84f
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2022-09-12 19:49:56 +0900

    feat: Add pull down to refresh
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/view/time_line_view.dart
+++ lib/view/time_line_view.dart
@@ -16,78 +16,82 @@ class TimeLineView extends GetView<TimeLineController> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Obx(
-        () => ListView.builder(
-          controller: _timeLineController.scrollController.value,
-          itemCount: _timeLineController.feeds.length,
-          itemBuilder: (context, index) {
-            Feed feed = _timeLineController.feeds[index];
+        () => RefreshIndicator(
+          onRefresh: () async {
+            await _timeLineController.refreshTimeLine();
+          },
+          child: ListView.builder(
+            controller: _timeLineController.scrollController.value,
+            itemCount: _timeLineController.feeds.length,
+            itemBuilder: (context, index) {
+              Feed feed = _timeLineController.feeds[index];
 
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      CircleAvatar(
-                        radius: 25,
-                        backgroundImage: NetworkImage(feed.actor!.profileThumbnailUrl!),
-                      ),
-                      const SizedBox(width: 5),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(feed.actor!.displayName!),
-                          Text(Utils.getTimeString(feed.createdAt!)),
-                        ],
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 4),
-                  SizedBox(
-                    width: double.infinity,
-                    child: Card(
-                      clipBehavior: Clip.antiAlias,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          if (feed.media != null)
-                            CarouselSlider.builder(
-                              itemCount: feed.media!.length,
-                              itemBuilder: (BuildContext context, int i, int pageViewIndex) => feed.media![i].isVideo
-                                  ? Stack(children: [
-                                      Image.network(
-                                        feed.media![i].previewUrl!,
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 25,
+                          backgroundImage: NetworkImage(feed.actor!.profileThumbnailUrl!),
+                        ),
+                        const SizedBox(width: 5),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(feed.actor!.displayName!),
+                            Text(Utils.getTimeString(feed.createdAt!)),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 4),
+                    SizedBox(
+                      width: double.infinity,
+                      child: Card(
+                        clipBehavior: Clip.antiAlias,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            if (feed.media != null)
+                              CarouselSlider.builder(
+                                itemCount: feed.media!.length,
+                                itemBuilder: (BuildContext context, int i, int pageViewIndex) => feed.media![i].isVideo
+                                    ? Stack(children: [
+                                        Image.network(
+                                          feed.media![i].previewUrl!,
+                                          fit: BoxFit.cover,
+                                          width: Get.width,
+                                          height: Get.height,
+                                        ),
+                                        const Positioned.fill(
+                                          child: Align(
+                                            alignment: Alignment.center,
+                                            child: Icon(
+                                              Icons.play_arrow,
+                                              color: Colors.white,
+                                              size: 50,
+                                            ),
+                                          ),
+                                        )
+                                      ])
+                                    : Image.network(
+                                        feed.media![i].originUrl!,
                                         fit: BoxFit.cover,
                                         width: Get.width,
                                         height: Get.height,
                                       ),
-                                      const Positioned.fill(
-                                        child: Align(
-                                          alignment: Alignment.center,
-                                          child: Icon(
-                                            Icons.play_arrow,
-                                            color: Colors.white,
-                                            size: 50,
-                                          ),
-                                        ),
-                                      )
-                                    ])
-                                  : Image.network(
-                                      feed.media![i].originUrl!,
-                                      fit: BoxFit.cover,
-                                      width: Get.width,
-                                      height: Get.height,
-                                    ),
-                              options: CarouselOptions(
-                                aspectRatio: feed.media![0].width!.toDouble() / feed.media![0].height!.toDouble(),
-                                enableInfiniteScroll: false,
-                                enlargeCenterPage: true,
-                                enlargeStrategy: CenterPageEnlargeStrategy.height,
-                                viewportFraction: 1.0,
+                                options: CarouselOptions(
+                                  aspectRatio: feed.media![0].width!.toDouble() / feed.media![0].height!.toDouble(),
+                                  enableInfiniteScroll: false,
+                                  enlargeCenterPage: true,
+                                  enlargeStrategy: CenterPageEnlargeStrategy.height,
+                                  viewportFraction: 1.0,
+                                ),
                               ),
-                            ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
                               child: ListView.builder(
                                 shrinkWrap: true,
                                 physics: const NeverScrollableScrollPhysics(),
@@ -114,9 +118,29 @@ class TimeLineView extends GetView<TimeLineController> {
                                 }),
                               ),
                             ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Wrap(
+                                spacing: 5,
+                                children: [
+                                  feed.liked! ? const Icon(Icons.favorite) : const Icon(Icons.favorite_outline),
+                                  Text(feed.likeCount.toString()),
+                                  const Icon(Icons.chat_bubble_outline),
+                                  Text(feed.commentCount.toString()),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(onPressed: (() async => await _timeLineController.refreshTimeLine())),
     );
   }
 }
