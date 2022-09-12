10170695d9930ca8d52bd6c5109e4ff038d3f9f4
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2022-09-12 16:54:28 +0900

    chore: Add latestFeed
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/controller/time_line_controller.dart
+++ lib/controller/time_line_controller.dart
@@ -6,6 +6,8 @@ class TimeLineController extends GetxController {
   final RxList<Feed> _feeds = <Feed>[].obs;
   RxList<Feed> get feeds => _feeds;
 
+  String latestFeed = "";
+
   @override
   void onReady() async {
     super.onReady();
@@ -14,6 +16,7 @@ class TimeLineController extends GetxController {
 
   Future<bool> refreshTimeLine() async {
     _feeds.assignAll(await StoryApi.getFeeds());
+    latestFeed = _feeds[_feeds.length - 1].id!;
     return Future.value(true);
   }
 }
