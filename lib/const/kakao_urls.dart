ebcdd7f2f48d6c7399b1c2308f289fcd2ed0b84f
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2022-09-12 15:57:38 +0900

    chore: Add feedUrl
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/const/kakao_urls.dart
+++ lib/const/kakao_urls.dart
@@ -1,2 +1,3 @@
 String loginUrl = "https://accounts.kakao.com/login/?continue=https://story.kakao.com/";
 String mainUrl = "https://story.kakao.com";
+String feedUrl = "https://story.kakao.com/a/feeds";
