274b60b75910e9cb4bbfb42e4943a6b8680201d4
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2022-09-12 19:48:11 +0900

    feat: Add Kakao Api
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

---
+++ lib/utils/kakao_api.dart
@@ -0,0 +1,30 @@
+import 'package:kakaostory/const/kakao_const.dart';
+import 'package:kakaostory/const/kakao_urls.dart' as KakaoUrl;
+import 'package:kakaostory/controller/auth_controller.dart';
+import 'package:kakaostory/model/kakao/emoticon.dart';
+
+import 'package:requests/requests.dart';
+
+class KakaoApi {
+  static Future<EmoticonAuth> getEmoticonCredential() async {
+    var result = await Requests.get(KakaoUrl.emoticonAuthUrl, headers: {
+      'cookie': AuthController.instance.kakaoCookies,
+      'authorization': "KakaoAK $kakaoApiKey",
+      'ka': ka,
+      'js-origin': KakaoUrl.mainUrl
+    });
+    return EmoticonAuth.fromJson(result.json());
+  }
+
+  static String getEmoticonUrl({required String id, required String resourceId}) {
+    String url = KakaoUrl.emoticonUrl;
+    url += "/$id/thum_${resourceId.padLeft(3, '0')}.png?";
+    url += "credential=${AuthController.emoticonAuth.auth!.credential}";
+    url += "&expires=${AuthController.emoticonAuth.auth!.expires}";
+    url += "&allow_referer=story.kakao.com";
+    url += "&signature=${AuthController.emoticonAuth.auth!.encodedSignature}";
+    url += "&path=${AuthController.emoticonAuth.auth!.path}";
+
+    return url;
+  }
+}
