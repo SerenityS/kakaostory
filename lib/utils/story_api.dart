ae4e949403462505630179bcced8c7baeefea919
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2022-09-12 19:48:38 +0900

    fix: Add header for get ContentDecorators
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/utils/story_api.dart
+++ lib/utils/story_api.dart
@@ -10,8 +10,8 @@ class StoryApi {
       feedUrl = "$feedUrl?since=$id";
     }
 
-    var result =
-        await Requests.get(feedUrl, headers: {'cookie': AuthController.instance.kakaoCookies, 'x-requested-with': "XMLHttpRequest"});
+    var result = await Requests.get(feedUrl,
+        headers: {'cookie': AuthController.instance.kakaoCookies, 'x-requested-with': "XMLHttpRequest", 'x-kakao-apilevel': "49"});
     return Feeds.fromJson(result.json()).feeds!;
   }
 }
